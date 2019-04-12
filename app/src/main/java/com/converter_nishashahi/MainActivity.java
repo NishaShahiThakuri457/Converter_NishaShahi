4b38a74e4b6db837117c66e89e77a399dd0587b4
NishaShahiThakuri457 <you@example.com>
2019-04-12 23:47:08 +0545

    Initial commit

---
+++ app/src/main/java/com/converter_nishashahi/MainActivity.java
@@ -0,0 +1,34 @@
+package com.converter_nishashahi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etNumber;
+    TextView tvWords;
+    Button btnConvert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etNumber= findViewById(R.id.etNumber);
+        tvWords= findViewById(R.id.tvWords);
+        btnConvert= findViewById(R.id.btnConvert);
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num;
+                num= Integer.parseInt(etNumber.getText().toString());
+
+                Converter_NishaShahi converter_nishaShahi= new Converter_NishaShahi(num);
+                tvWords.setText(converter_nishaShahi.Words());
+            }
+        });
+    }
+}
